c33b5c6ba0dd0793715e62529c8dc51958cc103e
YourAngel <angelyadiel8776@gmail.com>
2024-03-06 21:16:34 -0600

    Organized ideas and goals for project in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 /*
 Ideas organizer:
+Things to learn and implement for video game: (IS A LOT)
+
+1)Use SDL Lib for:
+-Graphic rendering
+-Audio
+-Hardware access
+-Windowing system
+
+2)Use Box2D lib for:
+-2D Physics
+
+3)EnTT obj and entities-
+-obj algorithm using ECS (entity-component system)
+
+4)aitoolkit lib for:
+-NPC AI intelligence
+
+5)Learn about game loops and how they work
+
+Overall: Graphic rendering, audio, hardware access, windowing system, Physics (2D), obj structure and algorithms,
+simple AI tools and game loops.
+
+Project may take a couple of months so probably going to do side projects to not fall behind in any way
 
 */
 int main(){
